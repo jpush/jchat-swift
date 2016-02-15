8b9661a688ad8cf8acf37c58aa8a5b3d6996155f
HuangMin <huangmin@jpush.cn>
2016-02-15 13:36:17 +0800

    新建 jchat swift 工程

---
+++ JChatSwift/JChatSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JChatSwift
+//
+//  Created by oshumini on 16/2/15.
+//  Copyright © 2016年 HXHG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
